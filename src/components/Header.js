560f3fb6ceba13e6c71da2ba57ac69f53e6667bd
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-10-18 09:28:58 -0300

    Header - Serachbar, icons, logo and etc... is ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,87 @@
-import { PageHeader, Button, Input, Space, Badge } from 'antd';
+import { PageHeader, Button, Input, Space, Badge, Tooltip } from "antd";
 import { useMoralis } from "react-moralis";
-import { Link } from 'react-router-dom';
-import './Header.css'
-import Amazon from "../images/logo.png";
-import USA from "../images/usa.png";
-import BookStore from "../images/bookstore.png";
-import {ShoppingCartOutlined, MenuOutlined} from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import MdLogo from "../images/mercadodex-logo.png";
+import {
+  ShoppingCartOutlined,
+  MenuOutlined,
+  UserOutlined,
+  WalletOutlined,
+} from "@ant-design/icons";
 
-const {Search } = Input;
-const categories = ["Comics", "Dictionaries", "Novels", "Fantasy", "Horror", "Adventure"];
+const { Search } = Input;
+const categories = [
+  "Comics",
+  "Dictionaries",
+  "Novels",
+  "Fantasy",
+  "Horror",
+  "Adventure",
+];
 
 const Header = () => {
   const { authenticate, account } = useMoralis();
-  return(
+  return (
     <div className="site-page-header-ghost-wrapper">
       <PageHeader
-        ghost={false}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        ghost={true}
         extra={[
           <>
-          <img src={Amazon} className="logo"></img>
-          <img src={BookStore} className="logo"></img>
-          <Search
-              placeholder="Find A Product"
+            <img src={MdLogo} className="logo"></img>
+            <Search
+              size="large"
+              placeholder="Estou procurando..."
               enterButton
-              className = "searchBar"
+              className="searchBar"
             />
-         <Button 
-         className="login"
-         key="1" 
-         type="primary" 
-         onClick={() => authenticate()}>
-          {account ? <span>{account.slice(0,5)}...</span> : <span>login</span>}
-          </Button>
-          <Space size={"large"}>
-              
-              <Badge count={0} showZero>
-                <span className="header-buttons">
-                  <ShoppingCartOutlined className="header-icon" />
-                  Cart
-                </span>
-              </Badge>
-              <Space className="header-buttons" size={"small"}>
-                <img src={USA} alt="region" className="flag"></img>▾
-              </Space>
-              
-            </Space>
-          </>
-        ]}>
-      </PageHeader>
-      <div className="site-page-subheader-ghost-wrapper">
-      <Space size={"middle"}>
-        <Space size={"small"} style={{fontWeight:"bold"}}>
-          <MenuOutlined />
-          Categories
-        </Space>
-        {categories.map((e) =>{
-          return(
-            <Link to="/categories" state={e} className="categories">
-              {e}
-            </Link>
-          )
 
-        })}
-      </Space>
+            <Button
+              key="1"
+              type="text"
+              icon={<UserOutlined className="personData" />}
+            ></Button>
+
+            <Tooltip title={account ? (<span>{account.slice(0, 8)}...</span>) : "Not logged yet"}>
+              <Button
+                key="2"
+                type="text"
+                icon={<WalletOutlined className="walletLogin" />}
+                onClick={() => authenticate()}
+              ></Button>
+            </Tooltip>
+
+            <Badge count={0} showZero>
+              <span className="header-buttons">
+                <ShoppingCartOutlined className="header-icon" />
+              </span>
+            </Badge>
+          </>,
+        ]}
+      ></PageHeader>
+
+      <div className="site-page-subheader-ghost-wrapper">
+        <Space size={"middle"}>
+          <Space size={"small"} style={{ fontWeight: "bold" }}>
+            <MenuOutlined />
+            Categories
+          </Space>
+          {categories.map((e) => {
+            return (
+              <Link to="/categories" state={e} className="categories">
+                {e}
+              </Link>
+            );
+          })}
+        </Space>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Header;
