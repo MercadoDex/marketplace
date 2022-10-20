3420da323d612b4d7e3e61d721da6e286b33e580
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-10-19 22:55:03 -0300

    test

---
+++ webpack.config.js
@@ -0,0 +1,1 @@
+'resolve.fallback: { "fs": false, "tls": false, "net": false, "path": false, "zlib": false, "http": false, "https": false, "stream": false, "crypto": false, "url": false, "os": false, assert: false }'
