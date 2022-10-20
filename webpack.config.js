d881d02c9cebe5d8e973a2ad5a820fb7bdbaa2de
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-10-19 23:09:41 -0300

    Final

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,1 @@
-module.exports = {
-    resolve: {
-      fallback: {
-        fs: false,
-        tls: false,
-        net: false,
-        path: false,
-        zlib: false,
-        http: false,
-        https: false,
-        stream: false,
-        crypto: false,
-        url: false,
-        os: false,
-        assert: false
-      },
-    },
-  };
+module.exports =  'resolve.fallback: { "http": false }'
