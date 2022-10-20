5e541ba623174dbbf2f53c2a3f593663aa077e4e
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-10-19 23:01:03 -0300

    Another fix?

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,18 @@
-'resolve.fallback: { "fs": false, "tls": false, "net": false, "path": false, "zlib": false, "http": false, "https": false, "stream": false, "crypto": false, "url": false, "os": false, assert: false }'
+module.exports = {
+    resolve: {
+      fallback: {
+        fs: false,
+        tls: false,
+        net: false,
+        path: false,
+        zlib: false,
+        http: false,
+        https: false,
+        stream: false,
+        crypto: false,
+        url: false,
+        os: false,
+        assert: false
+      },
+    },
+  };
